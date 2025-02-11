f0bdc8326f16d2f3d23324b5520bf76e1cd95c9f
r0zplay <r0zplay.biz@gmail.com>
2025-02-10 21:18:38 -0800

    First Commit

---
+++ src/main/java/net/roz/rozscurrency/RozsCurrency.java
@@ -0,0 +1,16 @@
+package net.roz.rozscurrency;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RozsCurrency implements ModInitializer {
+	public static final String MOD_ID = "rozs-currency";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/roz/rozscurrency/RozsCurrencyClient.java
@@ -0,0 +1,10 @@
+package net.roz.rozscurrency;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RozsCurrencyClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
